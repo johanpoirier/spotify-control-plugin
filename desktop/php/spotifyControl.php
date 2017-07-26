9a66de03249c6a8917d7633f68c5f6fed904bb14
Johan Poirier <johan.poirier@gmail.com>
2017-07-26 11:24:43 +0200

    Uses real labels as i18n keys

--- desktop/php/spotifyControl.php
+++ desktop/php/spotifyControl.php
@@ -1,20 +1,19 @@
 <?php
-if (!isConnect('admin')) {
-  throw new Exception('{{401 - Accès non autorisé}}');
-}
-$plugin = plugin::byId('spotifyControl');
-sendVarToJS('eqType', $plugin->getId());
-$eqLogics = eqLogic::byType($plugin->getId());
+    if (!isConnect('admin')) {
+      throw new Exception('{{401 - Accès non autorisé}}');
+    }
+    $plugin = plugin::byId('spotifyControl');
+    sendVarToJS('eqType', $plugin->getId());
+    $eqLogics = eqLogic::byType($plugin->getId());
 ?>
-
 <div class="row row-overflow">
     <div class="col-lg-2 col-md-3 col-sm-4">
         <div class="bs-sidebar">
             <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;"
-                   data-action="add"><i class="fa fa-plus-circle"></i> {{add_account}}</a>
-                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm"
-                                                                      placeholder="{{Rechercher}}" style="width: 100%"/>
+                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add">
+                    <i class="fa fa-plus-circle"></i> {{Ajouter un compte Spotify}}
+                </a>
+                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/>
                 </li>
               <?php
               foreach ($eqLogics as $eqLogic) {
@@ -26,13 +25,11 @@ $eqLogics = eqLogic::byType($plugin->getId());
         </div>
     </div>
 
-    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay"
-         style="border-left: solid 1px #EEE; padding-left: 25px;">
-        <legend>{{plugin_title}}</legend>
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+        <legend>{{Spotify Control}}</legend>
         <legend><i class="fa fa-cog"></i> {{Gestion}}</legend>
         <div class="eqLogicThumbnailContainer">
-            <div class="cursor eqLogicAction" data-action="add"
-                 style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+            <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
                 <i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
                 <br>
                 <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">{{Ajouter}}</span>
@@ -44,7 +41,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
                 <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676">{{Configuration}}</span>
             </div>
         </div>
-        <legend><i class="fa fa-table"></i> {{my_accounts}}</legend>
+        <legend><i class="fa fa-table"></i> {{Mes comptes}}</legend>
         <div class="eqLogicThumbnailContainer">
           <?php
           foreach ($eqLogics as $eqLogic) {
@@ -59,23 +56,32 @@ $eqLogics = eqLogic::byType($plugin->getId());
         </div>
     </div>
 
-    <div class="col-lg-10 col-md-9 col-sm-8 eqLogic"
-         style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-        <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i>
-            {{Sauvegarder}}</a>
-        <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i>
-            {{Supprimer}}</a>
-        <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i>
-            {{Configuration avancée}}</a>
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+        <a class="btn btn-success eqLogicAction pull-right" data-action="save">
+            <i class="fa fa-check-circle"></i> {{Sauvegarder}}
+        </a>
+        <a class="btn btn-danger eqLogicAction pull-right" data-action="remove">
+            <i class="fa fa-minus-circle"></i> {{Supprimer}}
+        </a>
+        <a class="btn btn-default eqLogicAction pull-right" data-action="configure">
+            <i class="fa fa-cogs"></i> {{Configuration avancée}}
+        </a>
         <ul class="nav nav-tabs" role="tablist">
-            <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab"
-                                       data-action="returnToThumbnailDisplay"><i
-                            class="fa fa-arrow-circle-left"></i></a></li>
-            <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab"
-                                                      data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a>
+            <li role="presentation">
+                <a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay">
+                    <i class="fa fa-arrow-circle-left"></i>
+                </a>
             </li>
-            <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i
-                            class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+            <li role="presentation" class="active">
+                <a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab">
+                    <i class="fa fa-tachometer"></i> {{Equipement}}
+                </a>
+            </li>
+            <li role="presentation">
+                <a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab">
+                    <i class="fa fa-list-alt"></i> {{Commandes}}
+                </a>
+            </li>
         </ul>
         <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
             <div role="tabpanel" class="tab-pane active" id="eqlogictab">
@@ -83,12 +89,10 @@ $eqLogics = eqLogic::byType($plugin->getId());
                 <form class="form-horizontal">
                     <fieldset>
                         <div class="form-group">
-                            <label class="col-sm-3 control-label">{{account_name}}</label>
+                            <label class="col-sm-3 control-label">{{Nom du compte Spotify}}</label>
                             <div class="col-sm-3">
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="id"
-                                       style="display : none;"/>
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="name"
-                                       placeholder="{{Nom de l'équipement template}}"/>
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;"/>
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement template}}"/>
                             </div>
                         </div>
                         <div class="form-group">
@@ -118,29 +122,27 @@ $eqLogics = eqLogic::byType($plugin->getId());
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-sm-3 control-label">{{client_id}}</label>
+                            <label class="col-sm-3 control-label">{{Client ID}}</label>
                             <div class="col-sm-3">
-                                <input type="text" class="eqLogicAttr configuration form-control"
-                                       data-l1key="configuration" data-l2key="clientId"/>
+                                <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="clientId"/>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-sm-3 control-label">{{client_secret}}</label>
+                            <label class="col-sm-3 control-label">{{Client Secret}}</label>
                             <div class="col-sm-3">
-                                <input type="text" class="eqLogicAttr configuration form-control"
-                                       data-l1key="configuration" data-l2key="clientSecret"/>
+                                <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="clientSecret"/>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-sm-3 control-label">{{redirect_uri}}</label>
+                            <label class="col-sm-3 control-label">{{Redirect URI}}</label>
                             <div class="col-sm-3">
-                                <input type="text" class="eqLogicAttr configuration form-control"
-                                       data-l1key="configuration" data-l2key="redirectUri"/>
+                                <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="redirectUri"/>
                             </div>
                         </div>
                     </fieldset>
                 </form>
             </div>
+
             <div role="tabpanel" class="tab-pane" id="commandtab">
                 <table id="table_cmd" class="table table-bordered table-condensed">
                     <thead>
